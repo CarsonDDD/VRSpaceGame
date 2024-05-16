77eefaae597e2a4e71bffd7fa35b1ce0928d8a01
Carson D <carson-dickinson@hotmail.com>
2024-05-16 15:26:57 -0500

    Add body and head movement

--- Assets/Scripts/RemoteControlledObject.cs
+++ Assets/Scripts/RemoteControlledObject.cs
@@ -18,14 +18,21 @@ using UnityEngine;
 public class RemoteControlledObject : MonoBehaviour
 {
     [SerializeField]
-    private Camera _camera;
+    private Transform headTransform;// aka the camera attached to the head
     private CharacterController _characterController;
 
-    public float speed = 0.0001f;
+    public float moveSpeed = 0.01f;
+	public float headSpeed = 0.09f;
 
-    // Joystick sim
-    public Vector2 joystick = new Vector2(0,0);// 1 and -1 are max intensity
-    //public float joystickReturnSpeed = 0.1f; // This may be a bad idea. This is done automatically through the real controller, no need to sim
+	//aaaah
+	private float currentXRotation = 0f;
+	private float currentYRotation = 0f;
+
+	// Joystick sim
+	public Vector2 moveStick = new Vector2(0,0);// 1 and -1 are max intensity
+    public Vector2 headStick = new Vector2(0,0);// rotation
+    public Vector2 MaxHeadRotationMagnitude = new Vector2(10,10);// cannot be negative
+    //public float moveReturnSpeed = 0.1f; // This may be a bad idea. This is done automatically through the real controller, no need to sim
 
 
     // Start is called before the first frame update
@@ -34,43 +41,93 @@ public class RemoteControlledObject : MonoBehaviour
         // Never fails
 		_characterController = GetComponent<CharacterController>();
 
+        //headObject.GetComponent<Camera>
+
 	}
     
     // Revert back to calling fixed update
     void Update()
     {
-        // joystick.y = z movement
-        joystick.Normalize();
-        _characterController.Move(new Vector3(joystick.x * speed, 0, joystick.y * speed));
+		// move.y = z movement
+		moveStick.Normalize();
+        _characterController.Move(new Vector3(moveStick.x * moveSpeed, 0, moveStick.y * moveSpeed));
 
-    }
+		headStick.Normalize();
+		//rotate based off x,y
+		// make sure final rotation x,y is within bounds
+
+		// y is actually x and x is actually y.
+		// to rotate in a direction we rotate on the other axis
+		currentYRotation += headStick.x * headSpeed;
+		currentXRotation += headStick.y * headSpeed;
+
+		//Debug.Log("PREnewRotation: " + newRotation);
+		currentXRotation = Mathf.Clamp(currentXRotation, -MaxHeadRotationMagnitude.y, MaxHeadRotationMagnitude.y);
+		currentYRotation = Mathf.Clamp(currentYRotation, -MaxHeadRotationMagnitude.x, MaxHeadRotationMagnitude.x);
+		//Debug.Log("POSTnewRotation: " + newRotation);
+		headTransform.localEulerAngles = new Vector3(currentXRotation, currentYRotation, 0f);
+	}
 
 	private void FixedUpdate()
 	{
-        if(Input.GetKey(KeyCode.UpArrow))
-        {
-            joystick.y += 1;
-        }
-        else if(Input.GetKey(KeyCode.DownArrow))
-		{
-			joystick.y -= 1;
-		}
-        else
-        {
-            joystick.y = 0;
-		}
+        // hold shift for camera
+        // only used for testing with keyboard
 
-		if(Input.GetKey(KeyCode.LeftArrow))
-		{
-			joystick.x -= 1;
-		}
-		else if(Input.GetKey(KeyCode.RightArrow))
-		{
-			joystick.x += 1;
+        if(Input.GetKey(KeyCode.RightShift))
+        {
+			if(Input.GetKey(KeyCode.UpArrow))
+			{
+				headStick.y -= 1;
+			}
+			else if(Input.GetKey(KeyCode.DownArrow))
+			{
+				headStick.y += 1;
+			}
+			else
+			{
+				headStick.y = 0;
+			}
+
+			if(Input.GetKey(KeyCode.LeftArrow))
+			{
+				headStick.x -= 1;
+			}
+			else if(Input.GetKey(KeyCode.RightArrow))
+			{
+				headStick.x += 1;
+			}
+			else
+			{
+				headStick.x = 0;
+			}
 		}
         else
         {
-			joystick.x = 0;
+			if(Input.GetKey(KeyCode.UpArrow))
+			{
+				moveStick.y += 1;
+			}
+			else if(Input.GetKey(KeyCode.DownArrow))
+			{
+				moveStick.y -= 1;
+			}
+			else
+			{
+				moveStick.y = 0;
+			}
+
+			if(Input.GetKey(KeyCode.LeftArrow))
+			{
+				moveStick.x -= 1;
+			}
+			else if(Input.GetKey(KeyCode.RightArrow))
+			{
+				moveStick.x += 1;
+			}
+			else
+			{
+				moveStick.x = 0;
+			}
 		}
 	}
 }
